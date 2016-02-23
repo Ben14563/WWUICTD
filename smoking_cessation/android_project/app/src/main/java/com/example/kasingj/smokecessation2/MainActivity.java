1ed48a63b3e5c80ba677715c6682d707d1e7f1a8
Alvin Ung <alvin.ung@live.com>
2016-02-23 00:57:30 -0800

    created simple dashboard screen

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kasingj.smokecessation2;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -7,6 +8,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -45,6 +48,11 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void goToDashboard (View view) {
+        Intent intent = new Intent (this, Dashboard.class);
+        startActivity(intent);
     }
 
     @Override
