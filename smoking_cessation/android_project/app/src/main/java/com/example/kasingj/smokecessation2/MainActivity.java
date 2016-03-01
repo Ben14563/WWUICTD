f93df23e8e126de9ae2f246e7f3cd376e9923b2a
James Kasinger <kasingj@students.wwu.edu>
2016-03-01 13:43:54 -0800

    working on an http client in Main Activity. Runs on background thread. Returns JSON data

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.kasingj.smokecessation2;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
@@ -9,6 +10,17 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import android.widget.Button;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        //creating fragments for intro activities
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tab_layout);
         tabLayout.addTab(tabLayout.newTab().setText("Current Habits"));
         tabLayout.addTab(tabLayout.newTab().setText("Goals"));
@@ -47,7 +59,18 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    /*Button signUpButton = (Button)findViewById(R.id.signUpButton); // example button
+    signUpButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            new JSONTask().execute("URL");
+        }
+    });*/
     }
+
+
+
+
 
     public void goToDashboard (View view) {
         Intent intent = new Intent (this, Dashboard.class);
@@ -78,5 +101,62 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+
+}
+
+ class JSONTask extends AsyncTask<String, String, String>{
+
+    @Override
+    protected String doInBackground(String... params){
+        HttpURLConnection connection =null;
+        BufferedReader reader=null;
+        try{
+            URL url = new URL(params[0]);
+            connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+            InputStream stream = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(stream));
+            String line ="";
+            StringBuffer buffer = new StringBuffer();
+
+            while((line = reader.readLine()) != null){
+                buffer.append(line);
+            }
+
+                String FinalJSON = buffer.toString();
+                JSONObject parentJSON = new JSONObject(FinalJSON);
+                JSONArray parentArray = parentJSON.getJSONArray("temporary"); //provide the key for the array
+                JSONObject finalObject = parentArray.getJSONObject(0); //use final object and pass it keys associated with values
+                String key1 = finalObject.getString("name_of_key1");
+                String key2 = finalObject.getString("name_of_key2");
+
+            return key1+" "+key2; // return the json as a string
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        } catch(JSONException e){
+            e.printStackTrace();
+        }finally {
+            if(connection!=null){
+                connection.disconnect();
+            }
+            try{
+                if(reader != null){
+                    reader.close();
+                }
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        super.onPostExecute(s);
+
     }
 }
