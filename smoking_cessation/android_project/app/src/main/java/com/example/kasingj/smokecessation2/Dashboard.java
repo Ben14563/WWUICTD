b8e9fa5c458771e8c53df51b4153d1d3b3ae11a9
Alvin Ung <alvin.ung@live.com>
2016-03-03 03:21:30 -0800

    created user stats table and database, mapped craving button, have not tested yet

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/Dashboard.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/Dashboard.java
@@ -1,12 +1,34 @@
 package com.example.kasingj.smokecessation2;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Dashboard extends AppCompatActivity {
+
+    static String username, time;
+    static int totDaysFree;
+    static int longStreak;
+    static int currStreak;
+    static int cravs;
+    static int cravsRes;
+    static int numSmokes;
+    static int lifeReg;
+    static double moneySaved;
+
+    Button CRAVE_BUTTON;
+    Button CRAVE_RES_BUTTON;
+    Button SMOKED_BUTTON;
+    Context ctx = this;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,8 +37,32 @@ public class Dashboard extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        // When "I'm Craving" button is pressed
+        CRAVE_BUTTON = (Button) findViewById(R.id.craveButton);
+        CRAVE_BUTTON.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                // pull from central database first
+
+                // update current stats and push
+                cravs += 1;
+                Date date = new Date();
+                SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+                time = sdf.format(date);
+
+                DatabaseOperations db = new DatabaseOperations(ctx);
+                db.addUserData(db, username, time, totDaysFree, longStreak, currStreak, cravs, cravsRes, numSmokes, moneySaved, lifeReg);
+                Toast.makeText(getBaseContext(), "Motivational Quote", Toast.LENGTH_LONG).show();
+            }
+        });
+
     }
 
+
+
+    // Navigation Buttons
     public void goToDashboard (View view) {
         Intent intent = new Intent (this, Dashboard.class);
         startActivity(intent);
---
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/DatabaseOperations.java
@@ -0,0 +1,67 @@
+package com.example.kasingj.smokecessation2;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import java.sql.Timestamp;
+
+/**
+ * Created by Alvin on 3/3/2016.
+ */
+public class DatabaseOperations extends SQLiteOpenHelper{
+
+    public static final int DATABASE_VERSION = 1;
+
+    // UserStats Query String
+    public String CREATE_USER_QUERY = "CREATE TABLE " + TableData.TableInfo.USER_TABLE_NAME + "(" +
+            " TEXT," + TableData.TableInfo.TOTAL_DAYS_FREE + " TEXT," + TableData.TableInfo.LONGEST_STREAK +
+            " TEXT," + TableData.TableInfo.CURRENT_STREAK + " TEXT," + TableData.TableInfo.NUM_CRAVINGS +
+            " TEXT," + TableData.TableInfo.CRAVINGS_RESISTED + " TEXT," + TableData.TableInfo.NUM_CIGS_SMOKED +
+            " TEXT," + TableData.TableInfo.MONEY_SAVED + " TEXT," + TableData.TableInfo.LIFE_REGAINED + " TEXT);";
+
+    // Create Database
+    public DatabaseOperations(Context context) {
+        super(context, TableData.TableInfo.DATABASE_NAME, null, DATABASE_VERSION);
+        Log.d("Database Operations", "Database created");
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sdb) {
+
+        // Create UserStats Table
+        sdb.execSQL(CREATE_USER_QUERY);
+        Log.d("Database Operations", "UserStats table created");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase arg0, int arg1, int arg2) {
+
+    }
+
+    // adding to UserStats Table
+    public void addUserData(DatabaseOperations dbop, String username, String time, int totsDayFree, int longStreak, int currStreak,
+                            int cravs, int cravsRes, int numSmokes, double moneySaved, int lifeReg) {
+
+        SQLiteDatabase sq = dbop.getWritableDatabase();
+        ContentValues cv = new ContentValues();
+
+        cv.put(TableData.TableInfo.USER_NAME, username);
+        cv.put(TableData.TableInfo.TIME, time);
+        cv.put(TableData.TableInfo.TOTAL_DAYS_FREE, totsDayFree);
+        cv.put(TableData.TableInfo.LONGEST_STREAK, longStreak);
+        cv.put(TableData.TableInfo.CURRENT_STREAK, currStreak);
+        cv.put(TableData.TableInfo.NUM_CRAVINGS, cravs);
+        cv.put(TableData.TableInfo.CRAVINGS_RESISTED, cravsRes);
+        cv.put(TableData.TableInfo.NUM_CIGS_SMOKED, numSmokes);
+        cv.put(TableData.TableInfo.MONEY_SAVED, moneySaved);
+        cv.put(TableData.TableInfo.LIFE_REGAINED, lifeReg);
+
+        sq.insert(TableData.TableInfo.USER_TABLE_NAME, null, cv);
+        Log.d("Database Operations", "One row inserted into UserStats Table");
+    }
+
+}
