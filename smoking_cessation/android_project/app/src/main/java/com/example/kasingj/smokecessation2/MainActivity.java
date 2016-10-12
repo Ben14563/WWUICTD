77345266c8ffc8cbbb34f72390bae9677ed7ed94
kasingj <kasingj@students.wwu.edu>
2016-10-12 10:12:02 -0700

    commit test

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/MainActivity.java
@@ -32,7 +32,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_main);
-
+        System.out.println("temp");
         //Toolbar toolbar = (Toolbar) findViewById(R.serverId.toolbar);
         //setSupportActionBar(toolbar);
     }
