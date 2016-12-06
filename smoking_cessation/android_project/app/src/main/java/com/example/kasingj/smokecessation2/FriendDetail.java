51e4a58b103a55d84ddcec025a667ff55bf15d74
James Kasinger <kasingj@students.wwu.edu>
2016-12-05 20:08:29 -0800

    added checkmark image in friend detail while comparing statics

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/FriendDetail.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/FriendDetail.java
@@ -2,9 +2,8 @@ package com.example.kasingj.smokecessation2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class FriendDetail extends AppCompatActivity {
 
@@ -28,8 +27,8 @@ public class FriendDetail extends AppCompatActivity {
 
         TextView userLifeRegained = (TextView) findViewById(R.id.life_regained_user);
         TextView buddyLifeRegained= (TextView) findViewById(R.id.life_regained_buddy);
-        userLifeRegained.setText( user.getLifeRegained() == null ? "-1" : user.getLifeRegained()+"" );
-        buddyLifeRegained.setText( friend.getLifeRegained() == null ? "-1" : friend.getLifeRegained()+"" );
+        userLifeRegained.setText( user.getLifeRegained() == null ? "-1" : user.getLifeRegained()+" min" );
+        buddyLifeRegained.setText( friend.getLifeRegained() == null ? "-1" : friend.getLifeRegained()+" min" );
 
         TextView userCurrentStreak = (TextView) findViewById(R.id.current_streak_user);
         TextView buddyCurrentStreak = (TextView) findViewById(R.id.current_streak_buddy);
@@ -39,8 +38,48 @@ public class FriendDetail extends AppCompatActivity {
         TextView userLongestStreak = (TextView) findViewById(R.id.longest_streak_user);
         TextView buddyLongestStreak = (TextView) findViewById(R.id.longest_streak_buddy);
         userLongestStreak.setText( user.getLongestStreak() == null ? "-1" : user.getLongestStreak()+"" );
-        buddyLongestStreak.setText( friend.getLongestStreak() == null ? "-1" : friend.getLongestStreak()+"" );
+        buddyLongestStreak.setText(friend.getLongestStreak() == null ? "-1" : friend.getLongestStreak() + "");
 
+        ImageView userMoneySavedImage = (ImageView) findViewById(R.id.money_saved_img_user);
+        ImageView buddyMoneySavedImage = (ImageView) findViewById(R.id.money_saved_img_buddy);
+//        userMoneySavedImage.setImageResource(R.drawable.checkmark);
+//
+//        ImageView userLifeRegainedImage = (ImageView) findViewById(R.id.life_regained_img_user);
+//        //ImageView buddyLifeRegainedImg = (ImageView) findViewById(R.id.life_regained_img_buddy);
+//        userLifeRegainedImage.setBackgroundResource(R.drawable.checkmark);
+
+        /* compare values and set accordingly*/
+        //money
+        ImageView win = ( Double.parseDouble(user.getMoneySaved()) > Double.parseDouble(friend.getMoneySaved()))
+                ? userMoneySavedImage : buddyMoneySavedImage;
+        win.setImageResource(R.drawable.checkmark);
+
+
+        //life regained
+        ImageView userLifeRegainedImage = (ImageView) findViewById(R.id.life_regained_img_user);
+        ImageView buddyLifeRegainedImage = (ImageView) findViewById(R.id.life_regained_img_buddy);
+
+        ImageView win2 = ( Integer.parseInt(user.getLifeRegained()) > Integer.parseInt(friend.getLifeRegained()))
+                ? userLifeRegainedImage : buddyLifeRegainedImage;
+        win2.setImageResource(R.drawable.checkmark);
+
+
+        //life regained
+        ImageView userCurrentStreakImage = (ImageView) findViewById(R.id.current_streak_img_user);
+        ImageView buddyCurrentStreakImage = (ImageView) findViewById(R.id.current_streak_img_buddy);
+
+        ImageView win3 = ( Integer.parseInt(user.getCurrentStreak()) >= Integer.parseInt(friend.getCurrentStreak()))
+                ? userCurrentStreakImage : buddyCurrentStreakImage;
+        win3.setImageResource(R.drawable.checkmark);
+
+        //longest streak
+        //life regained
+        ImageView userLongestStreakImage = (ImageView) findViewById(R.id.longest_streak_img_user);
+        ImageView buddyLongestStreakImage = (ImageView) findViewById(R.id.longest_streak_img_buddy);
+
+        ImageView win4 = ( Integer.parseInt(user.getLongestStreak()) >= Integer.parseInt(friend.getLongestStreak()))
+                ? userLongestStreakImage : buddyLongestStreakImage;
+        win4.setImageResource(R.drawable.checkmark);
 
 
 
