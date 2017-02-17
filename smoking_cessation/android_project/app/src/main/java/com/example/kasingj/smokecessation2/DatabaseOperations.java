63d4ed9cb9a0516629b401c4a961d0fb18b549bb
Alvin <alvin.ung@live.com>
2017-02-17 15:52:11 -0800

    changing user history table

--- smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/DatabaseOperations.java
+++ smoking_cessation/android_project/app/src/main/java/com/example/kasingj/smokecessation2/DatabaseOperations.java
@@ -20,8 +20,8 @@ public class DatabaseOperations extends SQLiteOpenHelper{
 
     // UserHistory Query String
     public String CREATE_USER_HISTORY_QUERY = "CREATE TABLE " + TableData.TableInfo.USER_HISTORY_TABLE_NAME + "(" +
-            TableData.TableInfo.ID + " INTEGER PRIMARY KEY," + TableData.TableInfo.USER_NAME + " TEXT," +
-            TableData.TableInfo.DATE + " TEXT," + TableData.TableInfo.SMOKED + " TEXT," +
+            TableData.TableInfo.ID + " INTEGER PRIMARY KEY," + TableData.TableInfo.TIME + " INTEGER PRIMARY KEY," +
+            TableData.TableInfo.USER_NAME + " TEXT," + TableData.TableInfo.DATE + " TEXT," + TableData.TableInfo.SMOKED + " TEXT," +
             TableData.TableInfo.CRAVING + " TEXT," + TableData.TableInfo.RESISTED + " TEXT);";
 
     // UserDemo Query String
